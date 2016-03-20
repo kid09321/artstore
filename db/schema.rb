ca086f6f8fdb3301ce375f53c12bc749188739c6
ScortiHuang <item0932@gmail.com>
2016-03-20 23:04:49 +0800

    fbtest1

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320130916) do
+ActiveRecord::Schema.define(version: 20160320143442) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "product_id"
