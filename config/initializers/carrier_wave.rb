af3bcbfc454f2667c5848a7a282db7596690831f
ScortiHuang <item0932@gmail.com>
2016-03-24 14:57:29 +0800

    把aws資訊放到figaro

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     'AKIAINJJFMZYBZTDDY5A',      # 你的 key
+      aws_access_key_id:     ENV['aws_access_key_id'],      # 你的 key
 
-      aws_secret_access_key: '2vqjBXPZa9cXTIu3oDLreLeyt88ioq48D3oiQt96',      # 你的 secret key
+      aws_secret_access_key: ENV['aws_secret_access_key'],      # 你的 secret key
 
       region:                'ap-northeast-1' # 你的 S3 bucket 的 Region 位置
 
