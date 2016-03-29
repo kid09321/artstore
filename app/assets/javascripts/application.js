4e1225bd774e867877eba5958eac3c388829a7c3
ScortiHuang <item0932@gmail.com>
2016-03-29 09:51:40 +0800

    carousel修改

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require bootstrap/dropdown
 //= require bootstrap/collapse
 //= require bootstrap/alert
